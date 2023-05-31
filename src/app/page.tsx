9be82b9223a5d2f9d491bee8e912ac084a5d96d7
viktorkulygin <mrastonone@gmail.com>
2023-05-31 14:46:50 +0300

    configured using svg as components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Text</Htag>
-      <Button appearance='primary'>Button</Button>
-      <Button appearance='ghost'>Button</Button>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
     </>
   );
 }
