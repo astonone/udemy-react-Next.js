917e5b507171ab94347125e8a1824c485f52f222
viktorkulygin <mrastonone@gmail.com>
2023-05-31 18:23:17 +0300

    added Tag component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import {JSX} from 'react';
-import {Button, HTag, PTag} from '@/app/components';
+import {Button, HTag, PTag, Tag} from '@/app/components';
 
 export default function Home(): JSX.Element {
     return (
@@ -11,6 +11,12 @@ export default function Home(): JSX.Element {
                 ipsa minima necessitatibus nisi possimus quisquam, suscipit voluptatibus!</PTag>
             <PTag size='m'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut earum nesciunt nihil nisi rem veniam?</PTag>
             <PTag size='l'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, repellendus.</PTag>
+            <Tag size='s'>Small</Tag>
+            <Tag size='m' color='red'>Red</Tag>
+            <Tag size='m' color='gray'>Gray</Tag>
+            <Tag size='s' color='green'>Green</Tag>
+            <Tag color='primary'>Primary</Tag>
+            <Tag color='primary' href='https://www.google.com'>Link</Tag>
         </>
     );
 }
